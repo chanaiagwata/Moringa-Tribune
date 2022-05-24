cfc8ec18c1c2c0d07fe80a217291461e8ea1cf21
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-24 16:36:16 +0300

    initial commit

---
+++ tribune/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+
+
+
+from django.contrib import admin
+
+# urlpatterns = [
+#     # Examples:
+#     # url(r'^$', 'tribune.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+
+#     url(r'^admin/', include(admin.site.urls)),
+# ]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^news/',include('news.urls'))
+]
